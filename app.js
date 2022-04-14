acdb0d999cce1870390638d83c9e23be12856a35
nitzanpap <nitzanpap@gmail.com>
2022-04-14 14:56:12 +0300

    Recreated  all previous html with js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", () => {
-    console.log("HTML page is loaded")
+    createAndDrawBoard()
+
+    function createAndDrawBoard() {
+        // Create and draw board-container table
+        const body = document.querySelector("body")
+        const boardContainer = document.createElement("table")
+        boardContainer.className = "board-container"
+        body.appendChild(boardContainer)
+
+        // Create and draw table rows
+        for (let i = 0; i < 8; i++) {
+            let row = document.createElement("tr")
+            row.className = "row"
+            row.id = "row" + (i + 1)
+            boardContainer.appendChild(row)
+            // Create and draw table cells
+            for (let j = 0; j < 8; j++) {
+                let col = document.createElement("td")
+                col.classList = "col col" + (i + 1)
+                row.appendChild(col)
+            }
+        }
+    }
 })
