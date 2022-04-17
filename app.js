b5abd7175de6d21dc817f02f83ef614745366da4
nitzanpap <nitzanpap@gmail.com>
2022-04-17 15:17:12 +0300

    Fixed redundant code, and moved imgs

--- app.js
+++ app.js
@@ -34,53 +34,39 @@ window.addEventListener("load", () => {
      * This function creates and draws the chess pieces
      */
     function createPieces() {
+        // Draw two special rows
         for (let i = 1; i <= 2; i++) {
             let row = document.querySelector("#row" + i)
-            for (let j = 1; j <= BOARD_SIZE; j++) {
-                // Special pieces row
-                if (i === 1) {
-                    // White row
-                    const tile = row.querySelector(".col" + j)
-                    const piece = document.createElement("img")
-                    tile.appendChild(piece)
-                    if (j === 1 || j === 8) piece.src = "imgs/pieces/white/rookW.png"
-                    else if (j === 2 || j === 7) piece.src = "imgs/pieces/white/knightW.png"
-                    else if (j === 3 || j === 6) piece.src = "imgs/pieces/white/bishopW.png"
-                    else if (j === 4) piece.src = "imgs/pieces/white/queenW.png"
-                    else if (j === 5) piece.src = "imgs/pieces/white/kingW.png"
+            let tileColor = "W"
+            // Runs twice - Once for the white pieces, and once for the black pieces
+            for (let k = 1; k <= 2; k++) {
+                // k alternates between creating the white and black pieces
+                if (k === 2) {
+                    tileColor = "B"
+                    row = document.querySelector("#row" + (BOARD_SIZE + 1 - i))
                 }
-                // Pawns row
-                if (i === 2) {
-                    // White row
-                    const tile = row.querySelector(".col" + j)
-                    console.log(tile)
-                    const piece = document.createElement("img")
-                    piece.src = "imgs/pieces/white/pawnW.png"
-                    tile.appendChild(piece)
-                }
-            }
-            row = document.querySelector("#row" + (BOARD_SIZE + 1 - i))
-            for (let j = 1; j <= BOARD_SIZE; j++) {
-                // Special pieces row
-                if (i === 1) {
-                    // Black row
-                    const tile = row.querySelector(".col" + j)
-                    const piece = document.createElement("img")
-                    tile.appendChild(piece)
-                    if (j === 1 || j === 8) piece.src = "imgs/pieces/black/rookB.png"
-                    else if (j === 2 || j === 7) piece.src = "imgs/pieces/black/knightB.png"
-                    else if (j === 3 || j === 6) piece.src = "imgs/pieces/black/bishopB.png"
-                    else if (j === 4) piece.src = "imgs/pieces/black/queenB.png"
-                    else if (j === 5) piece.src = "imgs/pieces/black/kingB.png"
-                }
-                // Pawns row
-                if (i === 2) {
-                    // Black row
-                    const tile = row.querySelector(".col" + j)
-                    console.log(tile)
-                    const piece = document.createElement("img")
-                    piece.src = "imgs/pieces/black/pawnB.png"
-                    tile.appendChild(piece)
+                for (let j = 1; j <= BOARD_SIZE; j++) {
+                    // Special pieces row
+                    if (i === 1) {
+                        const tile = row.querySelector(".col" + j)
+                        const piece = document.createElement("img")
+                        if (j === 1 || j === 8) piece.src = "imgs/pieces/rook" + tileColor + ".png"
+                        else if (j === 2 || j === 7)
+                            piece.src = "imgs/pieces/knight" + tileColor + ".png"
+                        else if (j === 3 || j === 6)
+                            piece.src = "imgs/pieces/bishop" + tileColor + ".png"
+                        else if (j === 4) piece.src = "imgs/pieces/queen" + tileColor + ".png"
+                        else if (j === 5) piece.src = "imgs/pieces/king" + tileColor + ".png"
+                        tile.appendChild(piece)
+                    }
+                    // Pawns row
+                    if (i === 2) {
+                        const tile = row.querySelector(".col" + j)
+                        console.log(tile)
+                        const piece = document.createElement("img")
+                        piece.src = "imgs/pieces/pawn" + tileColor + ".png"
+                        tile.appendChild(piece)
+                    }
                 }
             }
         }
