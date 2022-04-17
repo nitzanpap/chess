f01306ec504a3eeb4c84e937810a48bb997172c8
nitzanpap <nitzanpap@gmail.com>
2022-04-17 13:38:30 +0300

    Downloaded pieces images and updated js file

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", () => {
     const BOARD_SIZE = 8
+    const ASCII_NUM_OF_A = 65
     /**
      * This function creates and draws the chess board
      */
-    function createAndDrawBoard() {
+    function drawGame() {
+        createBoard()
+        // createPieces()
+    }
+    function createBoard() {
         // Create and draw board-container table
         const body = document.querySelector("body")
         const boardContainer = document.createElement("table")
@@ -11,19 +16,32 @@ window.addEventListener("load", () => {
         body.appendChild(boardContainer)
 
         // Create and draw table rows
-        for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let i = BOARD_SIZE; i >= 1; i--) {
             let row = document.createElement("tr")
             row.className = "row"
-            row.id = "row" + (i + 1)
+            row.id = "row" + i + ""
             boardContainer.appendChild(row)
             // Create and draw table cells
-            for (let j = 0; j < BOARD_SIZE; j++) {
+            for (let j = 1; j <= BOARD_SIZE; j++) {
                 let col = document.createElement("td")
-                col.classList = "col col" + (j + 1)
+                col.classList = "col col" + j
                 row.appendChild(col)
             }
         }
     }
+
+    /**
+     * This function creates and draws the chess pieces
+     */
+    function createPieces() {
+        for (let i = 1; i <= 2; i++) {
+            const row2 = document.querySelector("#row")
+            if (i === 1) {
+                for (let j = 0; j < BOARD_SIZE; j++) {}
+            }
+        }
+    }
+
     // Main app
-    createAndDrawBoard()
+    drawGame()
 })
