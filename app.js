8f770c3c74924d24590fbf4f5d77eed13753bedd
nitzanpap <nitzanpap@gmail.com>
2022-04-17 14:59:59 +0300

    Added pieces to board, still has redundant code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener("load", () => {
      */
     function drawGame() {
         createBoard()
-        // createPieces()
+        createPieces()
     }
     function createBoard() {
         // Create and draw board-container table
@@ -17,13 +17,13 @@ window.addEventListener("load", () => {
 
         // Create and draw table rows
         for (let i = BOARD_SIZE; i >= 1; i--) {
-            let row = document.createElement("tr")
+            const row = document.createElement("tr")
             row.className = "row"
             row.id = "row" + i + ""
             boardContainer.appendChild(row)
             // Create and draw table cells
             for (let j = 1; j <= BOARD_SIZE; j++) {
-                let col = document.createElement("td")
+                const col = document.createElement("td")
                 col.classList = "col col" + j
                 row.appendChild(col)
             }
@@ -35,9 +35,53 @@ window.addEventListener("load", () => {
      */
     function createPieces() {
         for (let i = 1; i <= 2; i++) {
-            const row2 = document.querySelector("#row")
-            if (i === 1) {
-                for (let j = 0; j < BOARD_SIZE; j++) {}
+            let row = document.querySelector("#row" + i)
+            for (let j = 1; j <= BOARD_SIZE; j++) {
+                // Special pieces row
+                if (i === 1) {
+                    // White row
+                    const tile = row.querySelector(".col" + j)
+                    const piece = document.createElement("img")
+                    tile.appendChild(piece)
+                    if (j === 1 || j === 8) piece.src = "imgs/pieces/white/rookW.png"
+                    else if (j === 2 || j === 7) piece.src = "imgs/pieces/white/knightW.png"
+                    else if (j === 3 || j === 6) piece.src = "imgs/pieces/white/bishopW.png"
+                    else if (j === 4) piece.src = "imgs/pieces/white/queenW.png"
+                    else if (j === 5) piece.src = "imgs/pieces/white/kingW.png"
+                }
+                // Pawns row
+                if (i === 2) {
+                    // White row
+                    const tile = row.querySelector(".col" + j)
+                    console.log(tile)
+                    const piece = document.createElement("img")
+                    piece.src = "imgs/pieces/white/pawnW.png"
+                    tile.appendChild(piece)
+                }
+            }
+            row = document.querySelector("#row" + (BOARD_SIZE + 1 - i))
+            for (let j = 1; j <= BOARD_SIZE; j++) {
+                // Special pieces row
+                if (i === 1) {
+                    // Black row
+                    const tile = row.querySelector(".col" + j)
+                    const piece = document.createElement("img")
+                    tile.appendChild(piece)
+                    if (j === 1 || j === 8) piece.src = "imgs/pieces/black/rookB.png"
+                    else if (j === 2 || j === 7) piece.src = "imgs/pieces/black/knightB.png"
+                    else if (j === 3 || j === 6) piece.src = "imgs/pieces/black/bishopB.png"
+                    else if (j === 4) piece.src = "imgs/pieces/black/queenB.png"
+                    else if (j === 5) piece.src = "imgs/pieces/black/kingB.png"
+                }
+                // Pawns row
+                if (i === 2) {
+                    // Black row
+                    const tile = row.querySelector(".col" + j)
+                    console.log(tile)
+                    const piece = document.createElement("img")
+                    piece.src = "imgs/pieces/black/pawnB.png"
+                    tile.appendChild(piece)
+                }
             }
         }
     }
