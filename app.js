bb1ea1784c8023a8ce3b9319083d309ee6071773
nitzanpap <nitzanpap@gmail.com>
2022-04-21 16:26:11 +0300

    Added pieces to the board and to the board array

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ runMainGameLoop()
 
 function runMainGameLoop() {
     createBoard()
+    createPieces()
 }
 
 // This function creates and draws the chess board
@@ -36,11 +37,14 @@ function createBoard() {
         // Create and draw table cells
         for (let j = 0; j <= BOARD_SIZE - 1; j++) {
             const td = document.createElement("td")
-            td.classList = "tdCol" + j
+            td.classList = "tdRow" + i + " tdCol" + j
             td.id = "td" + (j + i * BOARD_SIZE)
             tr.appendChild(td)
             // Add piece to board array
             addNewPieceToBoardArray(i, j, "e", "e")
+            td.addEventListener("click", () => {
+                handleTileClick(td)
+            })
         }
     }
     console.log(board)
@@ -48,6 +52,52 @@ function createBoard() {
 
 function addNewPieceToBoardArray(i, j, type, color) {
     board[i][j] = new Piece(i, j, type, color)
+}
+
+function createPieces() {
+    // Draw two special rows
+    for (let i = 0; i <= 1; i++) {
+        let tr = document.querySelector("#tr" + i)
+        let tileColor = "W"
+        // Runs twice - Once for the white pieces, and once for the black pieces
+        for (let k = 1; k <= 2; k++) {
+            // k alternates between creating the white and black pieces
+            if (k === 2) {
+                tileColor = "B"
+                tr = document.querySelector("#tr" + (BOARD_SIZE - 1 - i))
+            }
+            for (let j = 0; j <= BOARD_SIZE - 1; j++) {
+                const tdCol = tr.querySelector(".tdCol" + j)
+                let pieceType
+                // Special pieces row
+                if (i === 0) {
+                    // Add rook
+                    if (j === 0 || j === 7) pieceType = "rook"
+                    // Add knight
+                    else if (j === 1 || j === 6) pieceType = "knight"
+                    // Add bishop
+                    else if (j === 2 || j === 5) pieceType = "bishop"
+                    // Add queen
+                    else if (j === 3) pieceType = "queen"
+                    // Add king
+                    else if (j === 4) pieceType = "king"
+                }
+                // Pawns row
+                if (i === 1) pieceType = "pawn"
+                drawPieceInit(tdCol, pieceType, tileColor)
+                // Add pieces to board array
+                if (k === 1) addNewPieceToBoardArray(i, j, pieceType, tileColor)
+                else addNewPieceToBoardArray(BOARD_SIZE - 1 - i, j, pieceType, tileColor)
+            }
+        }
+    }
+}
+
+function drawPieceInit(tile, type, tileColor) {
+    const piece = document.createElement("img")
+    piece.className += "chess-piece " + type
+    piece.src = "imgs/pieces/" + type + tileColor + ".png"
+    tile.appendChild(piece)
 }
 
 /*
@@ -122,72 +172,5 @@ function selectTileClick(tile) {
     }
     tileSelected = tile
     tile.classList.add("selectedTile")
-}
-
-// Main app
-initGame()
-
-const tiles = Array.from(document.querySelectorAll(".tile"))
-
-tiles.forEach((tile) => {
-    tile.addEventListener("click", () => {
-        handleTileClick(tile)
-    })
-})
-
-// -------------------------------------------------------------------------
-
-function initGame() {
-    createBoard()
-    createPieces()
-}
-
-
-
-// This function creates and draws the chess pieces
-function createPieces() {
-    // Draw two special rows
-    for (let i = 0; i <= 1; i++) {
-        let row = document.querySelector("#row" + i)
-        let tileColor = "W"
-        // Runs twice - Once for the white pieces, and once for the black pieces
-        for (let k = 1; k <= 2; k++) {
-            // k alternates between creating the white and black pieces
-            if (k === 2) {
-                tileColor = "B"
-                row = document.querySelector("#row" + (BOARD_SIZE - 1 - i))
-            }
-            for (let j = 0; j <= BOARD_SIZE - 1; j++) {
-                const tile = row.querySelector(".col" + j)
-                let pieceType
-                // Special pieces row
-                if (i === 0) {
-                    // Add rook
-                    if (j === 0 || j === 7) pieceType = "rook"
-                    // Add knight
-                    else if (j === 1 || j === 6) pieceType = "knight"
-                    // Add bishop
-                    else if (j === 2 || j === 5) pieceType = "bishop"
-                    // Add queen
-                    else if (j === 3) pieceType = "queen"
-                    // Add king
-                    else if (j === 4) pieceType = "king"
-                }
-                // Pawns row
-                if (i === 1) pieceType = "pawn"
-                drawPieceInit(tile, pieceType, tileColor)
-                if (k === 1) addPieceToBoardArray(i, j, pieceType, tileColor)
-                else addPieceToBoardArray(BOARD_SIZE - 1 - i, j, pieceType, tileColor)
-            }
-        }
-    }
-}
-
-
-function drawPieceInit(tile, type, tileColor) {
-    const piece = document.createElement("img")
-    piece.className += "chess-piece " + type
-    piece.src = "imgs/pieces/" + type + tileColor + ".png"
-    tile.appendChild(piece)
 }
 */
